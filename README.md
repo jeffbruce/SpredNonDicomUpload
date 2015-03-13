91621bb155ce72800e9c6ba3a2da1f08ee5d761c
Jeff Bruce <jeffbruce@gmail.com>
2015-03-13 17:04:12 -0400

    Made the upload process interactive by default, and added to the documentation.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ---
-output: html_document
+output: pdf_document
 ---
 # NonDicomBatchUpload Documentation
 
@@ -8,6 +8,8 @@ output: html_document
 This is some brief documentation for the work I did for the Mouse Imaging Centre (MICe) and the Ontario Brain Institute (OBI).  The OBI is working on a big data solution for neuroscience data, called Brain-CODE, and it was my job to upload approximately 50 GB of mouse brain imaging data from MICe into the imaging side of Brain-CODE.  The imaging side of [Brain-CODE][1] uses the Stroke Patient Recovery Research Database (SPReD).
 
 Right now it uses an R script to generate the subject metadata, then uses a Python script to map the subject metadata to the XNAT XML schema, to extract other metadata, and to structure HTTP PUT web service calls to the XNAT REST API. 
+
+To learn how to use it, consult the Workflow header and make sure you have everything installed listed in the Dependencies header.  Development Notes / Rationales contains information for developers who might be interested in extending this work (maybe OOP-ifying it).
 
 ## Workflow
 
@@ -19,7 +21,11 @@ Right now it uses an R script to generate the subject metadata, then uses a Pyth
 
 4. Run the Python script, supplying your username and password, and it will upload subjects, sessions, scans, and the associated files to the project and site at the specified SPReD url.
 
-## Development Notes
+## Development Notes / Rationales
+
+#### Incorrect Field Mappings
+
+This section describes some fields that were difficult to deal with in the upload.
 
 - the difference between `study:start_time` and `vnmr:time_submitted` is unknown
 	- these fields are contained in the MINC header; it is unclear whether they would map to SPReD session and scan, respectively
@@ -29,12 +35,55 @@ Right now it uses an R script to generate the subject metadata, then uses a Pyth
 - the fov and matrix fields in XNAT are two dimensional (only x and y), however, all images at MICe are 3 dimensional without slices
 	- what is the best way of handling this behaviour?  possibly supply z dimension in number of frames
 - duration goes up to a maximum of around 16 hours
+- it's not clear what fields to use for mouse background, strain, and genotype
+
+#### Rationales
+
+- upload process is now interactive; when you're uploading such a large amount of data you should probably have someone overseeing the process
+  - it's pretty safe to run without interactivity because it will automatically turn off if it fails during one step of the process
+	- a bit torn on whether interactive mode should be optional / a flag
+- decided not to refactor string concatenation from + to ''.join of a list of strings because it would be less comprehensible to a newcomer
+- refactor methods so that instead of passing spred id and MINC filename around, you pass an object or namedtuple
+  - decided not to do this because it's just extra work really
+- the whole script exits when there's an error with processing a subject, and it spits out which subject the script had trouble processing along with what stage it had difficulty with (creating subject, session, scan, resource, file)
 - extending this module to work with any non-DICOM data would be difficult
 	- however, this script is still a useful code skeleton for people with their own non-DICOM data
-	- the python script assumes a number of things:
+	- the python script hard codes a number of things:
 		1. there is only 1 project, 1 session, 1 scan, 1 resource, and 1 file associated with a subject
-		2. metadata is generated with a separate R script, and must conform to a specific schema
+		2. metadata is generated with a separate R script, and must conform to a specific schema to be a valid upload
 	- all of the QA is specific to MICe
+
+## Dependencies
+
+- mincinfo
+- python packages (requests, numpy, pandas, subprocess)
+
+## TO DO
+
+- put the subject string into an upload note, so that it's obvious which spred_id corresponds to which filename?
+  - this may be necessary when uploading reconstructions
+- delete a subject/session/scan prior to PUT?
+	- what if an entity has extra content that it should not have?
+    - probably should delete it in this case
+- produce a log file of the metadata that was used for that upload along with the time of upload?
+	- that way someone can go back and look at a previous upload to see what exact files were uploaded
+  - what should the structure of this log file be?
+- verify that the metadata are accurate in the upload
+  - go through and check that the metadata are correct?
+	- any other better way of verifying this?
+    - you could GET the data and check it against what you added 
+- verify that the MINC file is accurate in the upload 
+	- is there information contained in the HTTP response body that I can use?
+	- use a checksum somehow?
+  - performing a GET and checking would be somewhat expensive
+	- manually spot check certain files with a trained eye?
+	- can't use the file size despite the MINC files being pretty huge files -- many of the files are the same size
+- how to increase upload speed?
+	- most of the time comes down to the actual upload of the file (about 10-15 secs for 75 MB file, and much longer for 450 MB)
+- write some proper use case documentation for this process
+  - this might help someone use it in the future
+- handle reconstructed images
+	- most places would download the MINC files, convert to NIfTI then analyze with FSL
 
 <!---
 References
--- SpredNonDicomUpload.py
+++ SpredNonDicomUpload.py
@@ -10,13 +10,41 @@ import sys
 import traceback
 
 
+# LAST WEEK
+# - successfully tested that the process works on a folder
+# - added some error checking, refactored some code to make it easier to follow
+# - identified ways of improving the script (see giant TO DO below)
+# - made the whole process interactive so you choose to upload a file each time 
+# - overall thoughts: definitely doing a good service, and cool how much data I can automatically upload, 
+# 					  but I want to get back to the Shiny app soon
+
+
 # TO DO
-# - test that the code works for a folder
-# - handle partial uploads (e.g. what happens if an upload fails part way through?  do you just repeat it?)
+# - maybe put the subject string into an upload note, so that it's obvious what spred_id corresponds to which filename?
+# 	- this may be necessary when uploading reconstructions
+# - delete a subject/session/scan prior to PUT?
+# 	- what if the entity has extra content that should not be there?
+# - verify that the metadata are accurate in the upload
+# 	- go through and check that the metadata are correct?
+# 	- any other better way of verifying this?
+# - produce a log file of the metadata that was used for that upload along with the time of upload?
+# 	- that way someone can go back and look at a previous upload to see what exact files were uploaded
+# - how to ensure that a file was truly successfuly uploaded? 
+# 	- is there information contained in the HTTP response body that I can use?
+# 	- use a checksum somehow?
+# 	- manually spot check certain files?
+# 	- can't use the file size despite the MINC files being pretty huge files -- many of them are the same size
+# - how to increase upload speed?
+# 	- most of the time is the actual upload of the file (about 10-15 secs for 75 MB file)
+# - refactor string concatenation from + to ''.join of a list of strings
+# 	- decided not to do this because it would be less comprehensible to a newcomer
+# - refactor methods so that instead of passing spred_id, filename around, you pass an object or namedtuple
+# - write some proper use case documentation for this process
+# - handle partial uploads (e.g. what happens if an upload fails partway through?  do you repeat the upload?)
+# 	- upload process is now interactive
+# 	- should you be able to toggle interactivity on or off?
 # - handle reconstructed images
-#	- most places would download the MINC files, convert to NIfTI then analyze with FSL
-# - integrate the metadata creation script (currently written in R) to be done in Python instead
-# 	- low priority
+# 	- most places would download the MINC files, convert to NIfTI then analyze with FSL
 
 
 # SET GLOBALS
@@ -43,15 +71,15 @@ pi_lastname = 'Lerch'
 # logon info 
 username = raw_input('SPReD username:')
 password = raw_input('SPReD password:')
-credentials = (username, password)
 # SPReD info
 base_url = 'https://spreddev.braincode.ca/spred/data/archive/projects/'
 session = requests.session()
+session.auth = (username, password)
 # .csv subject metadata file created in R with GenerateSubjectMetadata.R
 subject_metadata_file = 'SubjectMetadata.csv'
 
 
-def check_HTTP_status_code(action, response):
+def check_HTTP_status_code(action, response, subj_SPReD_ID):
 	'''Check that the web service request was successful.  
 	If it wasn't, exit program and print out error details.'''
 
@@ -69,14 +97,14 @@ def check_HTTP_status_code(action, response):
 def create_scan(MINC_filename, subj_SPReD_ID, session_name):
 	'''Creates a scan in SPReD, including the upload of any associated files.'''
 
-	# add constant key-values here
+	# # add constant key-values here
 	scan_params = {
-		# 'xsiType': 'xnat:mrScanData',
-		# 'xnat:mrScanData/type': scan_type,  # double check that this is the format SPReD wants
-		# 'xnat:mrScanData/quality': quality,
-		# 'xnat:mrScanData/scanner': scanner,
-		# 'xnat:mrScanData/scanner/manufacturer': scanner_manufacturer,
-		# 'xnat:mrScanData/fieldStrength': field_strength
+		'xsiType': 'xnat:mrScanData',
+		'xnat:mrScanData/type': scan_type,  # double check that this is the format SPReD wants
+		'xnat:mrScanData/quality': quality,
+		'xnat:mrScanData/scanner': scanner,
+		'xnat:mrScanData/scanner/manufacturer': scanner_manufacturer,
+		'xnat:mrScanData/fieldStrength': field_strength
 	}
 
 	resource_params = {
@@ -84,7 +112,7 @@ def create_scan(MINC_filename, subj_SPReD_ID, session_name):
 		'content': scan_type
 	}
 
-	# retrieve relevant fields from the mincheader
+	# # retrieve relevant fields from the mincheader
 	pslabel = get_minc_field('vnmr:pslabel', MINC_filename)
 	seqfil = get_minc_field('vnmr:seqfil', MINC_filename)
 	lpe = get_minc_field('vnmr:lpe', MINC_filename)
@@ -103,58 +131,58 @@ def create_scan(MINC_filename, subj_SPReD_ID, session_name):
 
 	# fov and matr don't allow 3D values
 	# fov must have integer values, yet MINC has them in float values
-	# if pslabel is not None:
-	# 	scan_params['xnat:mrScanData/series_description'] = pslabel
-	# if lpe is not None and nv is not None:
-	# 	scan_params['xnat:mrScanData/parameters/voxelRes/x'] = float(lpe) / float(nv)
-	# if lpe2 is not None and nv2 is not None:
-	# 	scan_params['xnat:mrScanData/parameters/voxelRes/y'] = float(lpe2) / float(nv2)
-	# if lro is not None and np is not None:
-	# 	scan_params['xnat:mrScanData/parameters/voxelRes/z'] = float(lro) / (float(np) / 2)
-	# if orient is not None:
-	# 	scan_params['xnat:mrScanData/parameters/orientation'] = orient
-	# # These are not handled well because they aren't 3D and sometimes they're int, sometimes they're float.
-	# # if lpe is not None:
-	# # 	scan_params['xnat:mrScanData/parameters/fov/x'] = float(lpe)
-	# # if lpe2 is not None:
-	# # 	scan_params['xnat:mrScanData/parameters/fov/y'] = float(lpe2)
-	# # if nv is not None:
-	# # 	scan_params['xnat:mrScanData/parameters/matrix/x'] = int(nv)
-	# # if nv2 is not None:
-	# # 	scan_params['xnat:mrScanData/parameters/matrix/y'] = int(nv2)
-	# if tr is not None:
-	# 	scan_params['xnat:mrScanData/parameters/tr'] = float(tr)
-	# if te is not None:
-	# 	scan_params['xnat:mrScanData/parameters/te'] = float(te)
-	# if ti is not None:
-	# 	scan_params['xnat:mrScanData/parameters/ti'] = float(ti)
-	# if flip_angle is not None:
-	# 	scan_params['xnat:mrScanData/parameters/flip'] = int(flip_angle)
-	# if seqfil is not None:
-	# 	scan_params['xnat:mrScanData/parameters/sequence'] = seqfil
-	# if file_type is not None:
-	# 	scan_params['xnat:mrScanData/parameters/imageType'] = file_type
+	if pslabel is not None:
+		scan_params['xnat:mrScanData/series_description'] = pslabel
+	if lpe is not None and nv is not None:
+		scan_params['xnat:mrScanData/parameters/voxelRes/x'] = float(lpe) / float(nv)
+	if lpe2 is not None and nv2 is not None:
+		scan_params['xnat:mrScanData/parameters/voxelRes/y'] = float(lpe2) / float(nv2)
+	if lro is not None and np is not None:
+		scan_params['xnat:mrScanData/parameters/voxelRes/z'] = float(lro) / (float(np) / 2)
+	if orient is not None:
+		scan_params['xnat:mrScanData/parameters/orientation'] = orient
+	# These are not handled well because they aren't 3D and sometimes they're int, sometimes they're float.
+	# if lpe is not None:
+	# 	scan_params['xnat:mrScanData/parameters/fov/x'] = float(lpe)
+	# if lpe2 is not None:
+	# 	scan_params['xnat:mrScanData/parameters/fov/y'] = float(lpe2)
+	# if nv is not None:
+	# 	scan_params['xnat:mrScanData/parameters/matrix/x'] = int(nv)
+	# if nv2 is not None:
+	# 	scan_params['xnat:mrScanData/parameters/matrix/y'] = int(nv2)
+	if tr is not None:
+		scan_params['xnat:mrScanData/parameters/tr'] = float(tr)
+	if te is not None:
+		scan_params['xnat:mrScanData/parameters/te'] = float(te)
+	if ti is not None:
+		scan_params['xnat:mrScanData/parameters/ti'] = float(ti)
+	if flip_angle is not None:
+		scan_params['xnat:mrScanData/parameters/flip'] = int(flip_angle)
+	if seqfil is not None:
+		scan_params['xnat:mrScanData/parameters/sequence'] = seqfil
+	if file_type is not None:
+		scan_params['xnat:mrScanData/parameters/imageType'] = file_type
 
 	# Create scan
 	url = base_url + project_name + '/subjects/' + subj_SPReD_ID + '/experiments/' + session_name + '/scans/scan' + str(int(scan_num))
-	resp = session.put(url, auth=credentials, params=scan_params)
+	resp = session.put(url, params=scan_params)
 
-	pdb.set_trace()
-
-	check_HTTP_status_code('creating scan', resp)
+	check_HTTP_status_code('creating scan', resp, subj_SPReD_ID)
 
 	# Create resource
 	url = base_url + project_name + '/subjects/' + subj_SPReD_ID + '/experiments/' + session_name + '/scans/scan' + str(int(scan_num)) + '/resources/' + str(int(resource_num))
-	resp = session.put(url, auth=credentials, params=resource_params)
+	resp = session.put(url, params=resource_params)
 
-	check_HTTP_status_code('creating resource', resp)
+	check_HTTP_status_code('creating resource', resp, subj_SPReD_ID)
 
 	# Upload a file
 	file_to_upload = {'file':open(MINC_filename, 'rb')}
 	url = base_url + project_name + '/subjects/' + subj_SPReD_ID + '/experiments/' + session_name + '/scans/scan' + str(int(scan_num)) + '/resources/' +  str(int(resource_num)) + '/files/'
-	resp = session.post(url, files=file_to_upload, auth=credentials, stream=True)
+	resp = session.post(url, files=file_to_upload, stream=True)
 
-	check_HTTP_status_code('creating file', resp)
+	check_HTTP_status_code('creating file', resp, subj_SPReD_ID)
+
+	notify_user_of_success(subj_SPReD_ID)
 
 
 def create_session(MINC_filename, subj_SPReD_ID, session_name):
@@ -163,23 +191,25 @@ def create_session(MINC_filename, subj_SPReD_ID, session_name):
 	# add constant key-values here
 	session_params = {
 		'xsiType': 'xnat:mrSessionData',
-		# 'xnat:mrSessionData/visit_id': visit_num, 
-		# # 'xnat:mrSessionData/project': project,  # won't recognize this for some reason
-		# 'xnat:mrSessionData/label': session_name
-		# 'xnat:mrSessionData/scanner': scanner,
-		# 'xnat:mrSessionData/scanner/manufacturer': scanner_manufacturer,
-		'xnat:mrSessionData/modality': modality 
-		# 'xnat:mrSessionData/fieldStrength': field_strength,
-		# 'xnat:mrSessionData/acquisition_site': acquisition_site
+		'xnat:mrSessionData/visit_id': int(visit_num), 
+		'xnat:mrSessionData/project': project_name,  # don't include this!  it generates a 403 for some reason
+		'xnat:mrSessionData/label': session_name,
+		'xnat:mrSessionData/scanner': scanner,
+		'xnat:mrSessionData/scanner/manufacturer': scanner_manufacturer,
+		'xnat:mrSessionData/modality': modality,
+		'xnat:mrSessionData/fieldStrength': field_strength,
+		'xnat:mrSessionData/acquisition_site': acquisition_site
 	}
 
-	# ni = float(get_minc_field('vnmr:ni', MINC_filename))
-	# nf = float(get_minc_field('vnmr:nf', MINC_filename))
-	# nfid = float(get_minc_field('vnmr:nfid', MINC_filename))
-	# nt = float(get_minc_field('vnmr:nt', MINC_filename))
-	# tr = float(get_minc_field('vnmr:tr', MINC_filename))
-	# etl = float(get_minc_field('vnmr:etl', MINC_filename))
+	ni = float(get_minc_field('vnmr:ni', MINC_filename))
+	nf = float(get_minc_field('vnmr:nf', MINC_filename))
+	nfid = float(get_minc_field('vnmr:nfid', MINC_filename))
+	nt = float(get_minc_field('vnmr:nt', MINC_filename))
+	tr = float(get_minc_field('vnmr:tr', MINC_filename))
+	etl = float(get_minc_field('vnmr:etl', MINC_filename))
 	
+	# duration isn't handled properly for some reason
+	# pretty sure this is a bug on XNAT's end -- I'll find out soon
 	# if ni is not None and nf is not None and nfid is not None and nt is not None and tr is not None and etl is not None:
 	# 	duration_secs = int(ni) * int(nf) * int(nfid) * int(nt) * int(tr)  / int(etl)
 	# 	# format duration value into xs:duration format
@@ -191,33 +221,32 @@ def create_session(MINC_filename, subj_SPReD_ID, session_name):
 	# 	xs_duration = 'PT' + str(int(hours)) + 'H' + str(int(minutes)) + 'M' + str(int(seconds)) + 'S'
 	# 	session_params['xnat:mrSessionData/duration'] = xs_duration
 	
-	# coil = get_minc_field('vnmr:rfcoil', MINC_filename)
-	# if coil is not None:
-	# 	session_params['xnat:mrSessionData/coil'] = coil
+	coil = get_minc_field('vnmr:rfcoil', MINC_filename)
+	if coil is not None:
+		session_params['xnat:mrSessionData/coil'] = coil
 
-	# datetime = get_minc_field('vnmr:time_submitted', MINC_filename)
-	# if datetime is not None:
-	# 	date = datetime[0:8]
-	# 	date = insert(date, '-', 6)
-	# 	date = insert(date, '-', 4)
-	# 	# date should conform to YYYY-MM-DD format
-	# 	session_params['xnat:mrSessionData/date'] = date
-	# 	time = datetime[9:len(datetime)]
-	# 	time = insert(time, ':', 4)
-	# 	time = insert(time, ':', 2)
-	# 	# time should conform to HH:MM:SS format
-	# 	session_params['xnat:mrSessionData/time'] = time
+	datetime = get_minc_field('vnmr:time_submitted', MINC_filename)
+	if datetime is not None:
+		date = datetime[0:8]
+		date = insert(date, '-', 6)
+		date = insert(date, '-', 4)
+		# date should conform to YYYY-MM-DD format
+		session_params['xnat:mrSessionData/date'] = date
+		time = datetime[9:len(datetime)]
+		time = insert(time, ':', 4)
+		time = insert(time, ':', 2)
+		# time should conform to HH:MM:SS format
+		session_params['xnat:mrSessionData/time'] = time
 	
-	# operator = get_minc_field('study:operator', MINC_filename)
-	# if operator is not None:
-	# 	session_params['xnat:mrSessionData/operator'] = operator
+	operator = get_minc_field('study:operator', MINC_filename)
+	if operator is not None:
+		session_params['xnat:mrSessionData/operator'] = operator
 
-	url = base_url + project_name + '/subjects/' + subj_SPReD_ID + '/experiments/session' + str(int(session_num))
-	resp = session.put(url, auth=credentials, params=session_params)
+	url = base_url + project_name + '/subjects/' + subj_SPReD_ID + '/experiments/' + session_name
 
-	pdb.set_trace()
+	resp = session.put(url, params=session_params)
 
-	check_HTTP_status_code('creating session', resp)
+	check_HTTP_status_code('creating session', resp, subj_SPReD_ID)
 
 
 def create_subject(MINC_filename, subj_SPReD_ID, row):
@@ -225,8 +254,8 @@ def create_subject(MINC_filename, subj_SPReD_ID, row):
 	row is a row in a pandas data frame.'''
 
 	subj_params = {
-		# 'pi_firstname': pi_firstname,
-		# 'pi_lastname': pi_lastname
+		'pi_firstname': pi_firstname,
+		'pi_lastname': pi_lastname
 	}
 	
 	if pd.notnull(row['Genotype']):
@@ -247,11 +276,9 @@ def create_subject(MINC_filename, subj_SPReD_ID, row):
 		subj_params['height'] = row['height']
 
 	url = base_url + project_name + '/subjects/' + subj_SPReD_ID
-	resp = session.put(url, auth=credentials, params=subj_params)
+	resp = session.put(url, params=subj_params)
 
-	pdb.set_trace()
-
-	check_HTTP_status_code('creating subject', resp)
+	check_HTTP_status_code('creating subject', resp, subj_SPReD_ID)
 
 
 def get_minc_field(field_name, MINC_filename):
@@ -274,6 +301,11 @@ def insert(original, new, pos):
 	return original[:pos] + new + original[pos:]
 
 
+def notify_user_of_success(subj_SPReD_ID):
+	'''Notifies the user of the successful creation of a collective subject, session, scan.'''
+	print 'Successful creation of subject: ' + subj_SPReD_ID
+
+
 def upload_data():
 	'''Loop through folders of mouse strains and create individual subjects to upload.'''
 
@@ -291,12 +323,18 @@ def upload_data():
 		MINC_filename = row['Filename']
 		subj_SPReD_ID = project_name + '_' + subj_num
 		session_name = subj_SPReD_ID + '_' + visit_num + '_' + 'SE' + session_num + '_' + modality
+		are_you_sure = ''
 
-		create_subject(MINC_filename, subj_SPReD_ID, row)
-
-		create_session(MINC_filename, subj_SPReD_ID, session_name)
-
-		create_scan(MINC_filename, subj_SPReD_ID, session_name)
+		while are_you_sure != 'y' and are_you_sure != 'n':
+			are_you_sure = raw_input("Are you sure you want to create subject %s" % subj_SPReD_ID + " and an associated session/scan? (y/n): ")
+			if are_you_sure == 'y':
+				create_subject(MINC_filename, subj_SPReD_ID, row)
+				create_session(MINC_filename, subj_SPReD_ID, session_name)
+				create_scan(MINC_filename, subj_SPReD_ID, session_name)
+			elif are_you_sure == 'n':
+				pass
+			else:
+				print 'Please enter either y or n'
 
 
 def zero_pad_subj_num(subj_num):
